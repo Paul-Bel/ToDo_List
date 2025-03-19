df891adf136f251002967ac854b70dfb6eab96c8
Paul-Bel <paul.bel.job@gmail.com>
2025-03-19 10:54:33 -0400

    added: manage Todo

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import {useState} from "react";
 
 function App() {
 
-    const [newTodo, setNewTodo] = useState("")
+    const [todoList, setTodoList] = useState([])
+
+    const handleAddTodo = (newTodo) => {
+        const id = Date.now()
+        setTodoList([...todoList, {id: id, title: newTodo}])
+    }
 
     return (
         <div>
             <h1>My Todos</h1>
-            <AddTodoForm onAddTodo={setNewTodo}/>
-            <p>{newTodo}</p>
-            <TodoList/>
+            <AddTodoForm onAddTodo={handleAddTodo}/>
+            <TodoList todoList={todoList}/>
         </div>
     )
 }
